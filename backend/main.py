07a7d7868dc1024828e176a6eaf0474bef8cb815
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2025-03-28 19:55:13 +0530

    vercel json added

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allow requests from the React frontend
+    allow_origins=["*"],  # Allow requests from the React frontend
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
